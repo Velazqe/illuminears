e42a92dbe62a23a689007103c7d25abea77b3220
Salokin <nicholas.dpoul@gmail.com>
2024-06-12 19:56:21 -0400

    got signup/login/logout working, home page and nav bar change content when logged in/out

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,29 @@
 import { useQuery } from "@apollo/client";
 import { Container, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import Auth from "../utils/auth";
+
 
 const Home = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
 
   return (
     <Container sx={{ display: 'flex', flexGrow: 1, alignItems: 'center', justifyContent: 'center', flexDirection: 'column', backgroundColor: 'background.secondary' }}>
-       <Typography variant="h1" component="h1" sx={{ margin: '20px'}}>
+       {Auth.loggedIn() ? (
+        <>
+        <Typography variant="h1" component="h1" sx={{ margin: '20px', textAlign: 'center'}}>
+       WELCOME <br></br>{Auth.getProfile().data.username}!
+      </Typography>
+      <Button color="inherit" onClick={logout} sx={{ margin: "20px", backgroundColor: 'primary.main'}}>
+        Log Out
+      </Button>
+      </>
+       ) : (
+        <>
+         <Typography variant="h1" component="h1" sx={{ margin: '20px'}}>
        WELCOME ILLUMINEARS!
       </Typography>
       <Button color="inherit" component={Link} to="/login" sx={{ margin: "20px", backgroundColor: 'primary.main'}}>
@@ -14,7 +31,10 @@ const Home = () => {
       </Button>
       <Button color="inherit" component={Link} to="/signup" sx={{ margin: "20px", backgroundColor: 'primary.main'}}>
         Sign Up
-      </Button>      
+      </Button>
+        </>
+       )}
+           
     </Container>
   );
 };
