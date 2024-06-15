8eb276d4ec9cde88232edd7ccc349876f656f569
Justin Ramos <Jramos20022@gmail.com>
2024-06-15 13:45:33 -0400

    deleted commented out code

--- client/src/components/SearchBar/searchBar.jsx
+++ client/src/components/SearchBar/searchBar.jsx
@@ -1,33 +1,3 @@
-// import React, { useState } from 'react';
-
-// const SearchBar = ({ onChange }) => {
-//   const [query, setQuery] = useState('');
-
-//   const handleInputChange = (e) => {
-//     setQuery(e.target.value);
-//   };
-
-//   const handleSearch = () => {
-//     console.log("Its running");
-//     onChange(query);
-//   };
-  
-//   return (
-//     <div className='searchbar'>
-//       <input
-//         type="text"
-//         value={query}
-//         onChange={handleInputChange}
-//         placeholder="Search..."
-//       />
-//       <button onClick={handleSearch}>Search</button>
-//     </div>
-//   );
-// };
-
-
-// export default SearchBar;
-
 import React, { useState } from 'react';
 import { Box, Button, TextField } from '@mui/material';
 
